778d40185251e22b63faffb9e2c185c779155ebf
michaelrizzo2 <m_rizzo@live.com>
2020-05-07 19:52:00 -0400

    finished render function

--- tic_tac_toe/src/Board.js
+++ tic_tac_toe/src/Board.js
@@ -37,4 +37,46 @@ class Board extends React.Component{
             XisNext:!this.state.XisNext
         })
     }
+
+    render()
+    {
+        const winner=utils.findWinner(this.state.squares);
+        const isFilled=utils.are_all_squares_clicked(this.state.squares);
+        let status;
+        if (winner)
+        {
+            status= "Winner: "+ winner;
+        }
+        else if (!winner && isFilled)
+        {
+            status="Game Drawn";
+        }
+        else
+        {
+            status=" Now" + (this.state.XisNext ? "X" : "O") + "'s turn";
+        }
+        return(
+            <div>
+            <div className ="status">{status}</div>
+            <div className ="board-row">
+                {this.rendersquare(0)}
+                {this.rendersquare(1)}
+                {this.rendersquare(2)}
+            </div>
+            <div className ="board-row">
+                {this.rendersquare(3)}
+                {this.rendersquare(4)}
+                {this.rendersquare(5)}
+            </div>
+            <div className ="board-row">
+                {this.rendersquare(6)}
+                {this.rendersquare(7)}
+                {this.rendersquare(8)}
+            </div>
+
+            </div>
+        )
+    }
 }
+
+export default Board;
